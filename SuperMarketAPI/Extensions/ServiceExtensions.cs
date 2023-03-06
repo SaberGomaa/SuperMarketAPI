c94ec71b33dc31653c7f8012adce68b8f7adbe35
sabegomaa261 <sabergomaa261@gmail.com>
2023-03-06 23:39:20 +0200

    CORS and IIS configuration

---
+++ SuperMarketAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿namespace SuperMarketAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+        //CORS
+        public static void ConfigureCors(this IServiceCollection services) =>
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", builder =>
+                builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            });
+
+        //IIS Coniguration
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+            });
+    }
+}
--- SuperMarketAPI/Program.cs
+++ SuperMarketAPI/Program.cs
@@ -1,13 +1,24 @@
+using Microsoft.AspNetCore.HttpOverrides;
+using SuperMarketAPI.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.ConfigureIISIntegration();
+builder.Services.ConfigureCors();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+    app.UseDeveloperExceptionPage();
+else
+    app.UseHsts();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -17,6 +28,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.All
+});
+app.UseCors("CorsPolicy");
 
 app.UseAuthorization();
 
