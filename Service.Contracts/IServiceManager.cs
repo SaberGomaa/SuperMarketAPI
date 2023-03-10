45a7bb0d04b1fb89e92d2d36237dced5b5eafca2
sabegomaa261 <sabergomaa261@gmail.com>
2023-03-10 22:00:19 +0200

    Service Manager

---
+++ Service.Contracts/IServiceManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service.Contracts
+{
+    public interface IServiceManager
+    {
+        public IAdminService Admin { get; }
+        public ICartService Cart { get; }
+        public IContactService Contact { get; }
+        public ICustomerService Customer { get; }
+        public IOrderService Order { get; }
+        public IProductService Product { get; }
+    }
+}
